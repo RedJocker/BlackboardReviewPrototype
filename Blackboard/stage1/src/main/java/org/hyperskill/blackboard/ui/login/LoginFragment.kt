5535da311588e5e71391229d092ec717545f31d7
RedJocker <mauricio.guaruja@gmail.com>
2023-03-19 19:52:14 -0300

    layout from main activity to fragment login

--- Blackboard/stage1/src/main/java/org/hyperskill/blackboard/ui/login/LoginFragment.kt
+++ Blackboard/stage1/src/main/java/org/hyperskill/blackboard/ui/login/LoginFragment.kt
@@ -1,5 +1,6 @@
 package org.hyperskill.blackboard.ui.login
 
+import android.app.Activity
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,15 +9,27 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import org.hyperskill.blackboard.R
+import org.hyperskill.blackboard.databinding.FragmentLoginBinding
+import org.hyperskill.blackboard.util.Extensions.showToast
 
 class LoginFragment : Fragment() {
 
     private val loginViewModel: LoginViewModel by viewModels { LoginViewModel.Factory }
-
+    private lateinit var fragmentLoginBinding: FragmentLoginBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        fragmentLoginBinding = FragmentLoginBinding.inflate(layoutInflater, container, false)
+
+        return fragmentLoginBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fragmentLoginBinding.loginBtn.setOnClickListener {
+            context?.showToast("Hello")
+        }
     }
 }
